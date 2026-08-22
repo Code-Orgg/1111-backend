bec1cbb113fdb7d9b10a81c40c85c1cd42aaf2ab
Orgg <nwaibedavid8@gmail.com>
2026-08-22 11:51:04 +0100

    Delete src directory

--- src/db.js
+++
@@ -1,8 +0,0 @@
-const { PrismaClient } = require("@prisma/client");
-
-// Reuse a single client instance (avoids exhausting DB connections in dev/hot-reload)
-const globalForPrisma = globalThis;
-const prisma = globalForPrisma.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-module.exports = prisma;
